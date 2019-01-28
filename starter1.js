e872c0fd3b08af40221fc998b483708fe5c58bba
Mohammed <moh.jameel178@gmail.com>
2019-01-28 10:47:07 +0300

    not completed

--- starter1.js
+++ starter1.js
@@ -1,3 +1,20 @@
 console.log('starter1.js linked');
 
 //code goes here
+function makeZoneGreen(event) {
+    console.log("mouse enterd!");
+    $(event.target).css('background-color', 'green');
+}
+function makeZoneGreenForEver(event) {
+    console.log("mouse clicked!");
+    $(event.target).css('background-color', 'green');
+    $(event.target).off("mouseleave")
+}
+function makeZoneWhite(event) {
+    console.log("mouse left!");
+    $(event.target).css('background-color', 'white');
+}
+
+$('.zone').on("mouseenter", makeZoneGreen);
+$('.zone').on("mouseleave", makeZoneWhite);
+$('.zone').on("click", makeZoneGreenForEver);
